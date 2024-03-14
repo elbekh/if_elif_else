361846624331620f97b36c3ead58e9ee5869ffcd
elbekh <157224637+elbekh@users.noreply.github.com>
2024-03-14 07:22:09 +0500

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,63 @@
+#                                    Masala 1
+# =============================================================================
+# son = int(input("Iltimos, bir son kiriting: "))
+# if son % 2 == 0 and son % 4 == 0: 
+#     print(f"{son} soni juft va 4 ga karrali.{son/4}")
+# else:
+#     print(f"{son} soni 4 ga karrali emas.")
+# =============================================================================
+
+#                                    Masala 2
+# =============================================================================
+# son = int(input("Iltimos, bir son kiriting: "))
+# if son % 2 > 0 and son % 5 == 0: 
+#     print(f"{son} soni toq va 5 ga karrali.{son/5}")
+# else:
+#     print(f"{son} soni 5 ga karrali emas.")
+# =============================================================================
+
+#                                    Masala 3
+# =============================================================================
+# son = int(input("Iltimos, bir son kiriting: "))
+# if son % 2 > 0 and son % 7 == 0: 
+#     print(f"{son} soni toq va 7 ga karrali.{son/7}")
+# else:
+#     print(f"{son} soni 7 ga karrali emas.")
+# =============================================================================
+
+#                                    Masala 4
+# =============================================================================
+# son = int(input("Iltimos, bir son kiriting: "))l
+# if son % 2 == 0 and son % 10 == 0: 
+#     print(f"{son} soni juft va 10 ga karrali.{son/10}")
+# else:
+#     print(f"{son} soni 10 ga karrali emas.")
+# =============================================================================
+#                                    Masala 5
+Masala Sharti: Berilgan tomonlari bo'yicha quti mavjud: AxBxC. MxN o'lchamli eshikdan o'tib ketishini aniqlovchi dastur tuzing'
+A =int(input("A tomonni kiriting: "))
+B =int(input("B tomonni kiriting: "))
+C =int(input("C tomonni kiriting: "))
+M =int(input("M tomonni kiriting: "))
+N =int(input("N tomonni kiriting: "))
+if (A+B>C and A+C>B and B+C>A) and (M>A and M>B and M>C and N>A,B,C):
+    print("Siz kiritgan sonlar hamma shartlarga to'g'ri keldi...")
+else:
+    print("Bu sonlardan bir  yoki bir nechtasi mos kelmayabdi. Qaytadan urinib ko'ring")
+#                                    Masala 6
+#                                    Masala 7
+#                                    Masala 8
+#                                    Masala 9
+#                                    Masala 10
+#                                    Masala 11
+#                                    Masala 12
+#                                    Masala 13
+#                                    Masala 14
+#                                    Masala 15
+#                                    Masala 16
+#                                    Masala 17
+#                                    Masala 18
+#                                    Masala 19
+#                                    Masala 20
+#                                    Masala 21
+#                                    Masala 22
